11b70cf0776a39f212e1e9bd3a0b6954b49f98fe
seeth <seeth@LAPTOP-NVUVQA1O>
2022-07-29 17:11:29 +0530

    hii

--- src/test/java/pack/Sample12.java
+++ src/test/java/pack/Sample12.java
@@ -5,5 +5,6 @@ public class Sample12 {
 		System.out.println("sai....hii guysS");
 		System.out.println("manish hi");
 		System.out.println("sai,sudheer,manish");
+		System.out.println("😀 😃 😄 😁 😆 😅 😂 🤣 🥲 ☺️ 😊 😇 🙂 🙃 😉 😌 😍 🥰 😘 😗 😙 😚 😋 😛 😝 😜 🤪 🤨 🧐 🤓 😎 🥸 🤩 🥳 😏 😒 😞 😔 😟 😕 🙁 ☹️");
 }
 }

dce5bba262672418e672c7e6083d1bb25f96e2de
Hp <Hp@DESKTOP-F4UK9QS>
2022-07-29 13:32:20 +0530

    FINAL COMMIT

--- src/test/java/practice/test3.java
+++ src/test/java/practice/test3.java
@@ -4,6 +4,7 @@ public class test3 {
 
 	public static void main(String[] args) {
 		System.out.println("test_2 passed");
+		System.out.println("Sudheer is going to TYSS");
 
 	}
 
--- src/test/java/practice/tester3.java
+++ src/test/java/practice/tester3.java
@@ -4,7 +4,7 @@ public class tester3 {
 
 	public static void main(String[] args) {
 		System.out.println("test_3 passed");
-
+         
 	}
 
 }

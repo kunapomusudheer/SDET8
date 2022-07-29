8563394adc6809a05647b1d0df88a8eb62bd429c
saikr <saikr@LAPTOP-C09100BS>
2022-07-29 17:09:42 +0530

    sai

--- src/test/java/pack/Sample12.java
+++ src/test/java/pack/Sample12.java
@@ -2,12 +2,12 @@ package pack;
 
 public class Sample12 {
 	public static void main(String[] args) {
-		
-	
-
 		System.out.println("manish hi");
 		System.out.println("sai,sudheer,manish");
 		System.out.println("saikrishna123");
+		
+		
+		System.out.println("super Sudheer");
 }
 
 }

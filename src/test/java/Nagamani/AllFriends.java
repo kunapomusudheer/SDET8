9173fbeb2ce2c159480c32b61308286fe44317de
LENOVO <LENOVO@DESKTOP-E57397J>
2022-07-29 18:12:50 +0530

    All Friends

---
+++ src/test/java/Nagamani/AllFriends.java
@@ -0,0 +1,17 @@
+package Nagamani;
+
+public class AllFriends {
+
+	public static void main(String[] args) {
+String head="Nagamani";
+String senior1="madhavi";
+String senior2="bhagya sri";
+String junior1="raman";
+String junior2="irfan";
+String associate="sudheer";
+String software="sai";
+String md="manish";
+
+	}
+
+}
